9a72a0ec0fc56b8f1148f5ae38a95bba5f9c6878
I-kong Fu <ikong@alumni.upenn.edu>
2013-07-11 21:46:26 -0400

    almost there?

--- web.js
+++ web.js
@@ -1,18 +1,22 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var content;
-fs.readFile('./index.html', function (err, data) {
-  if (err) throw err;
-  content = data;
-}); 
-
-
 app.get('/', function(request, response) {
-  response.send(content);
+
+
+var content;
+var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+//console.log(buffer.toString());
+
+
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
