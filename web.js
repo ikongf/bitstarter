269663e3b9666e22c260752e58caa6e2cb44a2b8
I-kong Fu <ikong@alumni.upenn.edu>
2013-07-11 21:16:36 -0400

    ugh

--- web.js
+++ web.js
@@ -2,15 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var content;
-fs.readFile('./index.html', function read(err, data) {
-  if (err) {
-    throw err;
-  }
+fs.readFile('./index.html', function (err, data) {
+  if (err) throw err;
   content = data;
-  
 }); 
 
-content = "test2";
 
 app.get('/', function(request, response) {
   response.send(content);
