b6819600ac2bbefda97af574cfed0adccc4b01a2
I-kong Fu <ikong@alumni.upenn.edu>
2013-07-11 20:59:05 -0400

    Updated web.js to try to read from file

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var stuff = fs.readFile('index.html');
+var stuff = 'test message';
 
 app.get('/', function(request, response) {
   response.send(stuff);
